5988330dbf169c7768ebb5005c985bc626971db0
garguelles <gerard.arguelles@gmail.com>
2024-07-04 18:15:28 +0800

    feat: floating diagnostic

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,14 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "gl", function()
+  local float = vim.diagnostic.config().float
+
+  if float then
+    local config = type(float) == "table" and float or {}
+    config.scope = "line"
+
+    vim.diagnostic.open_float(config)
+  end
+end, { desc = "toggle floating diagnostic" })
