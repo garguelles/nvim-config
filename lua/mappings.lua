5b0c0b32cdafc4158fe2b8a0270e70021c37f809
garguelles <gerard.arguelles@gmail.com>
2025-11-29 16:49:10 +0800

    feat: update starter config

---
+++ lua/mappings.lua
@@ -0,0 +1,23 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- floating diagnostics
+
+map("n", "gl", function()
+  local float = vim.diagnostic.config().float
+
+  if float then
+    local config = type(float) == "table" and float or {}
+    config.scope = "line"
+
+    vim.diagnostic.open_float(config)
+  end
+end, { desc = "toggle floating diagnostic" })
---
+++ lua/options.lua
@@ -0,0 +1,12 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+--
+vim.filetype.add { extension = { templ = "templ" } }
+vim.wo.wrap = false
+
+-- bun hot reload fix
+vim.o.backupcopy = "yes"
